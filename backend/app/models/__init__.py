6dc684ea97144fc8b0852d5d9cb91320807522bc
GalindoAsc <69743081+GalindoAsc@users.noreply.github.com>
2025-11-24 18:28:36 -0800

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.trip import Trip
-from app.models.space import Space
-from app.models.reservation import Reservation, ReservationSpace
-
-__all__ = ["User", "Trip", "Space", "Reservation", "ReservationSpace"]
