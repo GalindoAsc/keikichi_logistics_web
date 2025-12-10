6f282761b2ee64c5ef9109e4fa396fb404f6ec54
Daniel Galindo Ascencio <galindoasc@MacBook-Pro-de-Daniel.local>
2025-12-09 20:21:02 -0800

    deploy: update from remote script

--- sincronizacion-local.sh
+++ sincronizacion-local.sh
@@ -20,9 +20,10 @@ echo -e "${BLUE}>>> [LOCAL] Conectando al NAS (${NAS_HOST})...${NC}"
 
 # Usar sshpass para autenticación robusta
 echo -e "${GREEN}>>> Ejecutando comandos en NAS...${NC}"
+export SSHPASS="$NAS_PASS"
 
-# Force SSH to only use password authentication and ignore keys
-sshpass -p "$NAS_PASS" ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no -o PreferredAuthentications=password,keyboard-interactive "$NAS_USER@$NAS_HOST" \
+# Force SSH to only use password authentication and ignore keys. Enable verbose output.
+sshpass -e ssh -v -o StrictHostKeyChecking=no -o PubkeyAuthentication=no -o PreferredAuthentications=password,keyboard-interactive "$NAS_USER@$NAS_HOST" \
 "cd $NAS_DIR && echo '>>> Pulling changes...' && git pull && chmod +x deploy.sh && ./deploy.sh deploy"
 
 
